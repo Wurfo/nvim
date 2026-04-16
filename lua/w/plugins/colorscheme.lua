7fbf79d3e075913d28ceceb400bdc71f734cf23f
Wurfo <namiflowersbiz@gmail.com>
2026-04-16 14:52:29 +0200

    added colorschemes

--- lua/w/plugins/colorscheme.lua
+++ lua/w/plugins/colorscheme.lua
@@ -164,7 +164,41 @@ return {
 		"bjarneo/ash.nvim",
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme catppuccin-mocha]])
+			vim.cmd([[colorscheme vague]])
+		end,
+	},
+	{
+		"zenbones-theme/zenbones.nvim",
+		-- Optionally install Lush. Allows for more configuration or extending the colorscheme
+		-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+		-- In Vim, compat mode is turned on as Lush only works in Neovim.
+		dependencies = "rktjmp/lush.nvim",
+		lazy = false,
+		priority = 1000,
+		-- you can set set configuration options here
+		-- config = function()
+		--     vim.g.zenbones_darken_comments = 45
+		--     vim.cmd.colorscheme('zenbones')
+		-- end
+	},
+	{
+		"ember-theme/nvim",
+		name = "ember",
+		priority = 1000,
+		config = function()
+			require("ember").setup({
+				variant = "ember-soft", -- "ember" | "ember-soft" | "ember-light"
+			})
+			-- vim.cmd("colorscheme ember")
+		end,
+	},
+	{
+		"oskarnurm/koda.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- require("koda").setup({ transparent = true })
+			vim.cmd("colorscheme koda")
 		end,
 	},
 }
